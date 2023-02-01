64aaa93df46cab622b9b35a9ca21829e5ee4c3ce
Naum Tinga <naumting@gmail.com>
2023-02-01 13:11:37 +0200

    ErrorHandler class

--- src/app/restaurants/restaurants.service.ts
+++ src/app/restaurants/restaurants.service.ts
@@ -4,7 +4,8 @@ import {Restaurant} from './restaurant/restaurant.model';
 import {MEAT_API} from '../app.api';
 import {Observable} from 'rxjs';
 import 'rxjs/add/operator/map';
-
+import  'rxjs/add/operator/catch';
+import {ErrorHandler} from '../app.error-handler';
 
 @Injectable()
 export class RestaurantsService {
@@ -13,6 +14,13 @@ export class RestaurantsService {
 
   restaurants(): Observable<Restaurant[]> {
       return this.http.get(`${MEAT_API}/restaurants`)
-        .map(response => response.json());
+        .map(response => response.json())
+        .catch(ErrorHandler.handleError);
+  }
+
+  restaurantById(id: string): Observable<Restaurant> {
+    return this.http.get(`${MEAT_API}/restaurants/${id}`)
+      .map(response => response.json())
+      .catch(ErrorHandler.handleError);
   }
 }
