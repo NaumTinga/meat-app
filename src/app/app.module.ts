19c15da99510dedd855a06d09bb709fad1d430a7
Naum Tinga <naumting@gmail.com>
2023-02-01 13:34:48 +0200

    Menu Component Restaurant Detail component Reviews Component shopping cart Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,6 +13,11 @@ import {ROUTES} from './app.routes';
 import { RestaurantsComponent } from './restaurants/restaurants.component';
 import { RestaurantComponent } from './restaurants/restaurant/restaurant.component';
 import {RestaurantsService} from './restaurants/restaurants.service';
+import { RestaurantDetailComponent } from './restaurant-detail/restaurant-detail.component';
+import { MenuComponent } from './restaurant-detail/menu/menu.component';
+import { ShoppingCartComponent } from './restaurant-detail/shopping-cart/shopping-cart.component';
+import { MenuItemComponent } from './restaurant-detail/menu-item/menu-item.component';
+import { ReviewsComponent } from './restaurant-detail/reviews/reviews.component';
 
 @NgModule({
   declarations: [
@@ -22,6 +27,11 @@ import {RestaurantsService} from './restaurants/restaurants.service';
     AboutComponent,
     RestaurantsComponent,
     RestaurantComponent,
+    RestaurantDetailComponent,
+    MenuComponent,
+    ShoppingCartComponent,
+    MenuItemComponent,
+    ReviewsComponent,
   ],
   imports: [
     BrowserModule,
